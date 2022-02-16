8072ab82ba65152183def0e4ef38b3c70ac2d850
Ardhikarestu <ardhikayoviyanto@gmail.com>
2022-02-16 23:12:06 +0700

    fiks, create function sum and substraction

--- main.go
+++ main.go
@@ -4,7 +4,9 @@ import "fmt"
 
 func main() {
 	fmt.Println("Message from branch main ...")
-	fmt.Println("Message from feature development")
+	fmt.Println("Message from feature development...")
+	println("1 + 5 = ", sumtwonumbers(1, 5))
+	println("2 * 5 = ", subtractiontwonumber(2, 5))
 }
 
 func sumtwonumbers(x int, y int) int {
